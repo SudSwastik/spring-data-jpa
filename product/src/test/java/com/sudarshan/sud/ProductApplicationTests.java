ab0e6757879b9621f36800c09dee5d0230b183f2
SudarshanKumar <sudarshan4309@gmail.com>
2020-03-28 23:19:47 +0530

    Created Model Class For Product

---
+++ product/src/test/java/com/sudarshan/sud/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudarshan.sud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
