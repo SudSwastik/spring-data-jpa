370bb61f995080f1fa956b94021b6b7af01f8865
SudarshanKumar <sudarshan4309@gmail.com>
2020-03-29 13:30:59 +0530

    Created Model And Repository Interface.

---
+++ jpqlandnativesql/src/main/java/com/sudarshan/sud/JpqlandnativesqlApplication.java
@@ -0,0 +1,12 @@
+package com.sudarshan.sud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpqlandnativesqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpqlandnativesqlApplication.class, args);
+	}
+}
