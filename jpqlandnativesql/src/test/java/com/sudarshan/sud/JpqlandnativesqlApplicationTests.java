111b9a8cb1b2ef45309482f76cf22d55f2708222
SudarshanKumar <sudarshan4309@gmail.com>
2020-03-29 13:58:10 +0530

    Added Unit Test For Student Creation

---
+++ jpqlandnativesql/src/test/java/com/sudarshan/sud/JpqlandnativesqlApplicationTests.java
@@ -0,0 +1,54 @@
+package com.sudarshan.sud;
+
+import java.util.List;
+
+import com.sudarshan.sud.model.Student;
+import com.sudarshan.sud.repository.StudentRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpqlandnativesqlApplicationTests {
+
+	@Autowired
+	StudentRepository repository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testCreateStudent() {
+		Student student = new Student();
+		student.setFirstName("John");
+		student.setLastName("Ferguson");
+		student.setScore(88);
+
+		Student student2 = new Student();
+		student2.setFirstName("Bill");
+		student2.setLastName("Gates");
+		student2.setScore(75);
+
+		repository.save(student);
+		repository.save(student2);
+	}
+
+}
+
+
+
+
+
+
+
+
+
